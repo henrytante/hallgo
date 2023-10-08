319fba7e76d4d7afc57801ebf4b6a7978da869f7
henry <143973064+beniop@users.noreply.github.com>
2023-10-07 22:26:35 -0300

    Add files via upload

---
+++ installer_termux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if ! command -v go &> /dev/null; then
+    echo "Golang não instalado. Instalando..."
+    pkg install golang
+fi
+go build -o hall
+sudo cp hall /data/data/com.termux/files/usr/bin
+clear
+echo 'Hall foi instalado com sucesso. Execute com o comando "hall"'
